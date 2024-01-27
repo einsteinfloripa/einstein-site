bb1afc4704b1dcd5f1e77af89d3b1538e2251dbf
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2024-01-27 11:12:46 -0300

    chore: add paternships section

--- src/components/Text/style.ts
+++ src/components/Text/style.ts
@@ -1,4 +1,4 @@
-import { tv, type VariantProps } from "tailwind-variants";
+import { tv, type ClassProp, type VariantProps } from "tailwind-variants";
 
 export const text = tv({
   base: "font-roboto",
@@ -6,10 +6,19 @@ export const text = tv({
     color: {
       primary: "text-primary",
     },
+    size: {
+      title: "text-3xl",
+    },
   },
   defaultVariants: {
     color: "primary",
   },
+  compoundVariants: [
+    {
+      size: "title",
+      class: "font-bold",
+    },
+  ],
 });
 
-export type TextProps = VariantProps<typeof text>;
+export type TextProps = VariantProps<typeof text> & ClassProp;
